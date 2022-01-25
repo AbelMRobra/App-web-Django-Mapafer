2defa66d423c23f2ad813e3aeb5efc4fd28e00ae
arobra-tije <arobra@tije.travel>
2022-01-25 12:12:30 -0300

    JS client

---
+++ sinergia/administracion/static/js/f_client.js
@@ -0,0 +1,48 @@
+
+
+async function service_crear_cliente() {
+
+    const url = `${document.getElementById("host").value}api/api_clientes/`;
+    
+    var respuesta = await fetch(url ,{
+        method: "POST",
+        headers: {
+            'X-CSRFToken' : `${document.getElementById("token").value}`,
+            'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+            'nombre' : document.getElementById("nombre").value,
+            'apellido' : document.getElementById("apellido").value,
+            'score' : document.getElementById("score").value,
+            'email' : document.getElementById("email").value,
+            'cuil' : document.getElementById("cuil").value,
+            'telfono' : document.getElementById("telfono").value,
+            'direccion' : document.getElementById("direccion").value,
+            'empresa' : document.getElementById("empresa").value,
+            'username' : document.getElementById("username").value,
+            'password' : document.getElementById("password").value,
+        })
+
+    });
+
+    var response = await respuesta.json();
+    var status = await respuesta.status;
+    return validar_respuesta_creacion(response, status)
+}
+
+function validar_respuesta_creacion(response, status){
+    
+    if (status >= 200 && status <300){
+        sweet_alert("Cliente creado", "success")
+    } else {
+        console.log(Object.keys(response))
+        let errors = Object.keys(response)
+        for (let i=0; i <= errors.length -1; i++){
+            console.log(response[errors[i]])
+        }
+
+        sweet_alert("No aceptado", "warning")
+ 
+    }
+}
