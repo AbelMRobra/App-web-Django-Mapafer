06a5bb49cd084379649aa6fd184bb35c1137f92a
Abel <sebastian@letsmade.co>
2022-03-29 18:08:33 -0300

    Fix viewset pagos

--- sinergia/administracion/viewsets/viewsets_pagos.py
+++ sinergia/administracion/viewsets/viewsets_pagos.py
@@ -6,8 +6,21 @@ from rest_framework.decorators import action, permission_classes
 from administracion.models import Pagos
 from administracion.serializers import serializers_pagos
 
+from ..funciones.f_prestamos import *
+
 class PagosViewset(viewsets.ModelViewSet):
 
     queryset = Pagos.objects.all()
     serializer_class = serializers_pagos.PagosSerializers
     permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        prestamos_cuotas_pagos(request.data['prestamo'])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    
