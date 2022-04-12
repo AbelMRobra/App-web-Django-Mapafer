dffb808db7cea1093159f4e98a17909c374cd0b9
Abel <sebastian@letsmade.co>
2022-04-12 17:37:50 -0300

    Detailes

--- sinergia/administracion/views_programa/views_caja.py
+++ sinergia/administracion/views_programa/views_caja.py
@@ -1,10 +1,11 @@
 from django.db.models import Sum
 from django.shortcuts import render
-from administracion.models import PagosProveedores, Pagos, Prestamos
+from administracion.models import PagosProveedores, Pagos, MovimientoContable
 
 def principal_caja(request):
     context = {}
     context['pagos'] = Pagos.objects.all().values("fecha").order_by("fecha").annotate(total_monto=Sum('monto'))
     context['pagos_proveedores'] = PagosProveedores.objects.all().values("fecha").order_by("fecha").annotate(total_monto=Sum('monto'))
+    context['movimientos'] = MovimientoContable.objects.all()
 
     return render(request, "caja/caja_principal.html", context)
